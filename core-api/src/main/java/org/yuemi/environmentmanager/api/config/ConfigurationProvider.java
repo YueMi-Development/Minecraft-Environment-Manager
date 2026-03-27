1f3977bb3578703414d8c1289a7a9b7b788dc075
Muhammad Tamir <muhammad.tamir@gmail.com>
2026-03-27 16:13:15 +0700

    Add Javadoc to Environment and Config APIs
    Add Javadoc documentation to core API classes and interfaces: EnvironmentManager (class-level and method docs for constructor, loadConfig, applyEnvironmentKeys, getEnvironmentKeys), ConfigurationManager (class-level, constructor, registerProvider, load, save) and ConfigurationProvider (interface and method docs including default File overloads). Purely documentation/comments; no functional changes to logic. Affected files: EnvironmentManager.java, ConfigurationManager.java, ConfigurationProvider.java.

--- core-api/src/main/java/org/yuemi/environmentmanager/api/config/ConfigurationProvider.java
+++ core-api/src/main/java/org/yuemi/environmentmanager/api/config/ConfigurationProvider.java
@@ -7,17 +7,48 @@ import org.spongepowered.configurate.ConfigurationNode;
 import java.io.File;
 import java.nio.file.Path;
 
+/**
+ * Interface for configuration providers that can load and save configuration nodes.
+ */
 public interface ConfigurationProvider {
 
+    /**
+     * Loads a configuration node from a path.
+     *
+     * @param path the path to load from
+     * @return the loaded configuration node
+     * @throws ConfigurateException if an error occurs during loading
+     */
     @NotNull
     ConfigurationNode load(@NotNull Path path) throws ConfigurateException;
 
+    /**
+     * Saves a configuration node to a path.
+     *
+     * @param path the path to save to
+     * @param node the configuration node to save
+     * @throws ConfigurateException if an error occurs during saving
+     */
     void save(@NotNull Path path, @NotNull ConfigurationNode node) throws ConfigurateException;
 
+    /**
+     * Loads a configuration node from a file.
+     *
+     * @param file the file to load from
+     * @return the loaded configuration node
+     * @throws ConfigurateException if an error occurs during loading
+     */
     default @NotNull ConfigurationNode load(@NotNull File file) throws ConfigurateException {
         return load(file.toPath());
     }
 
+    /**
+     * Saves a configuration node to a file.
+     *
+     * @param file the file to save to
+     * @param node the configuration node to save
+     * @throws ConfigurateException if an error occurs during saving
+     */
     default void save(@NotNull File file, @NotNull ConfigurationNode node) throws ConfigurateException {
         save(file.toPath(), node);
     }
